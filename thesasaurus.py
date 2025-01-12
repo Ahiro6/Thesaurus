8d4c5c598feea895a33bf034747f167068b9c82c
Adriaan <hiro6hamada6@gmail.com>
2025-01-12 21:27:10 +0200

    built exe

--- thesasaurus.py
+++ thesasaurus.py
@@ -29,6 +29,8 @@ def getDef(word):
     elif len(difflib.get_close_matches(word.upper(), data.keys(), cutoff=0.8)) > 0:
         word = difflib.get_close_matches(word.upper(), data.keys(), cutoff=0.8)[0]
         definition = data.get(word)
+        
+        definition.insert(0, "Did you mean " + word + "? \n")
         return definition
 
     elif word.title() in data:
@@ -42,10 +44,10 @@ def getDef(word):
         return definition
 
     elif word == "":
-        definition = "Please enter a word."
+        definition = ["Please enter a word."]
         return definition
     else:
-        definition = "This word does not exist. Double check your entry."
+        definition = ["This word does not exist. Double check your entry."]
         return definition
 
 
@@ -60,20 +62,20 @@ def update_window():
         id += 1
 
 
-b = Button(window, text="Enter", command=update_window, width=10)
-b.grid(row=4, column=0, columnspan=2)
+b = Button(window, text="Enter", command=update_window, width=20)
+b.grid(row=4, column=2, columnspan=4)
 b_y = Button(window, text="Yes")
 b_n = Button(window, text="No")
 
 e_value = StringVar()
-e = Entry(window, textvariable=e_value)
-e.grid(row=1, column=1)
+e = Entry(window, textvariable=e_value, width=30)
+e.grid(row=1, column=2, columnspan=4)
 
 t = Listbox(window, width=40, height=10)
-t.grid(row=2, column=1)
+t.grid(row=2, column=1, columnspan=6)
 
 sc = Scrollbar(window, orient="horizontal")
-sc.grid(row=3, column=0, columnspan=2)
+sc.grid(row=3, column=1, columnspan=6)
 
 sb = Scrollbar(window)
 sb.grid(column=0, row=0, rowspan=4)
@@ -85,7 +87,7 @@ t.configure(yscrollcommand=sb.set)
 sb.configure(command=t.yview)
 
 la = Label(window, text="Type a word:")
-la.grid(row=0, column=0, columnspan=2)
+la.grid(row=0, column=3, columnspan=2)
 
 
 window.mainloop()
